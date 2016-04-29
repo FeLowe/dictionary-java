7ea47bcb5729ad53f960469c53c5362dd2ddb61d
Fernanda Lowe <fe_lowe@hotmail.com>
2016-04-29 15:26:50 -0700

    fail test for getId on Definition objet as method has not yeat been created

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -2,11 +2,22 @@ import java.util.ArrayList;
 
 public class Definition {
   private String mDefinition;
+  private static ArrayList<Definition> allDefinitions = new ArrayList<Definition>();
 
   public Definition(String definition) {
     mDefinition = definition;
+    allDefinitions.add(this);
+
   }
   public String getDefinition(){
     return mDefinition;
   }
+  public static ArrayList<Definition> all(){
+    return allDefinitions;
+  }
+  public static void clear() {
+  allDefinitions.clear();
+  }
+
+
 }
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -18,6 +18,24 @@ public class DefinitionTest {
   public void getDefinition_InstantiatesWithDefinition_Home() {
     Definition testDefinition = new Definition("The place where one lives permanently");
     assertEquals("The place where one lives permanently", testDefinition.getDefinition());
-
   }
+  @Test
+  public void allDefinitions_returnsAllInstancesOfDefinition_true() {
+    Definition firstDefinition = new Definition("The place where one lives permanently");
+    Definition secondDefinition = new Definition("An institution for educating children.");
+    assertTrue(Definition.all().contains(firstDefinition));
+    assertTrue(Definition.all().contains(secondDefinition));
+  }
+    @Test
+    public void clear_emptiesAllDefinitionsFromList_0() {
+      Definition testDefinition = new Definition("The place where one lives permanently");
+      Definition.clear();
+      assertEquals(Definition.all().size(), 0);
+  }
+    @Test
+    public void getId_definitionsInstantiateWithAnId_1() {
+    Definition testDefinition = new Definition("The place where one lives permanently");
+    assertEquals(1, testDefinition.getId());
+  }
+
 }

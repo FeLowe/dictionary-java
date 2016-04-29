c39e4ac5dca1efdadabee084f9dd1c754313bb46
Fernanda Lowe <fe_lowe@hotmail.com>
2016-04-29 15:14:03 -0700

    pass test on getDefinition method

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 
 public class Definition {
+  private String mDefinition;
 
   public Definition(String definition) {
-
+    mDefinition = definition;
   }
-
+  public String getDefinition(){
+    return mDefinition;
+  }
 }

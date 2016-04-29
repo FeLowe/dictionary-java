5990bb1aaa40673a1a936e3699eaddd63cf69a83
Fernanda Lowe <fe_lowe@hotmail.com>
2016-04-29 15:11:42 -0700

    fail getDefinition on @Test as method has not yet been created

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -9,9 +9,15 @@ public class DefinitionTest {
   // }
 
   @Test
- public void definition_instantiatesCorrectly_true() {
-   Definition testDefinition = new Definition("The place where one lives permanently");
-   assertEquals(true, testDefinition instanceof Definition);
- }
+  public void definition_instantiatesCorrectly_true() {
+    Definition testDefinition = new Definition("The place where one lives permanently");
+    assertEquals(true, testDefinition instanceof Definition);
+  }
 
+  @Test
+  public void getDefinition_InstantiatesWithDefinition_Home() {
+    Definition testDefinition = new Definition("The place where one lives permanently");
+    assertEquals("The place where one lives permanently", testDefinition.getDefinition());
+
+  }
 }
